99f37f335d4ce0f20a1a34f585a6a400f55e8509
Canna Wen <cannawen@gmail.com>
2012-03-23 17:19:17 -0400

    client-side SSL handshake

--- client.c
+++ client.c
@@ -16,6 +16,11 @@
 #define HOST "localhost"
 #define PORT 8765
 
+#define BUFSIZZ 1024
+static char *REQUEST_TEMPLATE=
+   "GET / HTTP/1.0\r\nUser-Agent:"
+   "EKRClient\r\nHost: %s:%d\r\n\r\n";
+
 /* use these strings to tell the marker what is happening */
 #define FMT_CONNECT_ERR "ECE568-CLIENT: SSL connect error\n"
 #define FMT_SERVER_INFO "ECE568-CLIENT: %s %s %s\n"
@@ -31,6 +36,14 @@
 
 BIO *bio_err=0;
 static char *pass;
+static int require_server_auth=1;
+
+int err_exit(string)
+  char *string;
+  {
+    fprintf(stderr,"%s\n",string);
+    exit(0);
+  }
 
 /* Print SSL errors and exit*/
 int berr_exit(string)
@@ -100,7 +113,123 @@ SSL_CTX *initialize_ctx(keyfile,password)
     return ctx;
   }
 
+void check_cert(ssl,host)
+  SSL *ssl;
+  char *host;
+  {
+    X509 *peer;
+    char peer_CN[256];
+    
+    if(SSL_get_verify_result(ssl)!=X509_V_OK)
+      berr_exit("Certificate doesn't verify");
 
+    /*Check the cert chain. The chain length
+      is automatically checked by OpenSSL when
+      we set the verify depth in the ctx */
+
+    /*Check the common name*/
+    peer=SSL_get_peer_certificate(ssl);
+    X509_NAME_get_text_by_NID
+      (X509_get_subject_name(peer),
+      NID_commonName, peer_CN, 256);
+    if(strcasecmp(peer_CN,host))
+    err_exit
+      ("Common name doesn't match host name");
+  }
+
+int tcp_connect(host,port)
+  char *host;
+  int port;
+  {
+    struct hostent *hp;
+    struct sockaddr_in addr;
+    int sock;
+    
+    if(!(hp=gethostbyname(host)))
+      berr_exit("Couldn't resolve host");
+    memset(&addr,0,sizeof(addr));
+    addr.sin_addr=*(struct in_addr*)
+      hp->h_addr_list[0];
+    addr.sin_family=AF_INET;
+    addr.sin_port=htons(port);
+
+    if((sock=socket(AF_INET,SOCK_STREAM,
+      IPPROTO_TCP))<0)
+      err_exit("Couldn't create socket");
+    if(connect(sock,(struct sockaddr *)&addr,
+      sizeof(addr))<0)
+      err_exit("Couldn't connect socket");
+    
+    return sock;
+  }
+
+
+static int http_request(ssl)
+  SSL *ssl;
+  {
+    char *request=0;
+    char buf[BUFSIZZ];
+    int r;
+    int len, request_len;
+    
+    /* Now construct our HTTP request */
+    request_len=strlen(REQUEST_TEMPLATE)+
+      strlen(HOST)+6;
+    if(!(request=(char *)malloc(request_len)))
+      err_exit("Couldn't allocate request");
+    snprintf(request,request_len,REQUEST_TEMPLATE,
+      HOST,PORT);
+
+    /* Find the exact request_len */
+    request_len=strlen(request);
+
+    r=SSL_write(ssl,request,request_len);
+    switch(SSL_get_error(ssl,r)){      
+      case SSL_ERROR_NONE:
+        if(request_len!=r)
+          err_exit("Incomplete write!");
+        break;
+        default:
+          berr_exit("SSL write problem");
+    }
+    
+    /* Now read the server's response, assuming
+       that it's terminated by a close */
+    while(1){
+      r=SSL_read(ssl,buf,BUFSIZZ);
+      switch(SSL_get_error(ssl,r)){
+        case SSL_ERROR_NONE:
+          len=r;
+          break;
+        case SSL_ERROR_ZERO_RETURN:
+          goto shutdown;
+        case SSL_ERROR_SYSCALL:
+          fprintf(stderr,
+            "SSL Error: Premature close\n");
+          goto done;
+        default:
+          berr_exit("SSL read problem");
+      }
+
+      fwrite(buf,1,len,stdout);
+    }
+    
+  shutdown:
+    r=SSL_shutdown(ssl);
+    switch(r){
+      case 1:
+        break; /* Success */
+      case 0:
+      case -1:
+      default:
+        berr_exit("Shutdown failed");
+    }
+    
+  done:
+    SSL_free(ssl);
+    free(request);
+    return(0);
+  }
 
 
 
@@ -114,7 +243,12 @@ int main(int argc, char **argv)
   struct hostent *host_entry;
   char buf[256];
   char *secret = "What's the question?";
-  
+      SSL_CTX *ctx;
+    SSL *ssl;
+    BIO *sbio;
+    extern char *optarg;
+    int c;
+
   /*Parse command line arguments*/
   
   switch(argc){
@@ -147,8 +281,23 @@ int main(int argc, char **argv)
 
 
 
-  initialize_ctx("alice.pem","password");
+  ctx = initialize_ctx("alice.pem","password");
 
+    /* Connect the TCP socket*/
+    sock=tcp_connect(host,port);
+
+    /* Connect the SSL socket */
+    ssl=SSL_new(ctx);
+    sbio=BIO_new_socket(sock,BIO_NOCLOSE);
+    SSL_set_bio(ssl,sbio,sbio);
+
+    if(SSL_connect(ssl)<=0)
+      berr_exit("SSL connect error");
+    if(require_server_auth)
+      check_cert(ssl,host);
+
+    /* Now make our HTTP request */
+    http_request(ssl);
 
 
 
