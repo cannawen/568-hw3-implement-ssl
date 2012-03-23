781a42461d6064debe099c92934613818f736985
Canna Wen <cannawen@gmail.com>
2012-03-23 16:38:58 -0400

    Client-side initialization of stuff

--- client.c
+++ client.c
@@ -7,6 +7,11 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <netinet/tcp.h>
+#include <fcntl.h>
+#include <signal.h>
 
 #define HOST "localhost"
 #define PORT 8765
@@ -19,6 +24,87 @@
 #define FMT_EMAIL_MISMATCH "ECE568-CLIENT: Server Email doesn't match\n"
 #define FMT_NO_VERIFY "ECE568-CLIENT: Certificate does not verify\n"
 #define FMT_INCORRECT_CLOSE "ECE568-CLIENT: Premature close\n"
+
+
+
+
+
+BIO *bio_err=0;
+static char *pass;
+
+/* Print SSL errors and exit*/
+int berr_exit(string)
+  char *string;
+  {
+    BIO_printf(bio_err,"%s\n",string);
+    ERR_print_errors(bio_err);
+    exit(0);
+  }
+
+static void sigpipe_handle(int x){
+}
+
+static int password_cb(char *buf,int num,
+  int rwflag,void *userdata)
+  {
+    if(num<strlen(pass)+1)
+      return(0);
+
+    strcpy(buf,pass);
+    return(strlen(pass));
+  }
+
+SSL_CTX *initialize_ctx(keyfile,password)
+  char *keyfile;
+  char *password;
+  {
+    SSL_METHOD *meth;
+    SSL_CTX *ctx;
+    
+    if(!bio_err){
+      /* Global system initialization*/
+      SSL_library_init();
+      SSL_load_error_strings();
+      
+      /* An error write context */
+      bio_err=BIO_new_fp(stderr,BIO_NOCLOSE);
+    }
+
+    /* Set up a SIGPIPE handler */
+    signal(SIGPIPE,sigpipe_handle);
+    
+    /* Create our context*/
+    meth=SSLv23_method();
+    ctx=SSL_CTX_new(meth);
+
+    /* Load our keys and certificates*/
+    if(!(SSL_CTX_use_certificate_chain_file(ctx,
+      keyfile)))
+      berr_exit("Can't read certificate file");
+
+    pass=password;
+    SSL_CTX_set_default_passwd_cb(ctx,
+      password_cb);
+    if(!(SSL_CTX_use_PrivateKey_file(ctx,
+      keyfile,SSL_FILETYPE_PEM)))
+      berr_exit("Can't read key file");
+
+    /* Load the CAs we trust*/
+    if(!(SSL_CTX_load_verify_locations(ctx,
+      "568ca.pem",0)))
+      berr_exit("Can't read CA list");
+#if (OPENSSL_VERSION_NUMBER < 0x00905100L)
+    SSL_CTX_set_verify_depth(ctx,1);
+#endif
+    
+    return ctx;
+  }
+
+
+
+
+
+
 
 int main(int argc, char **argv)
 {
@@ -55,6 +141,18 @@ int main(int argc, char **argv)
     fprintf(stderr,"Couldn't resolve host");
     exit(0);
   }
+
+
+
+
+
+
+  initialize_ctx("alice.pem","password");
+
+
+
+
+
 
   memset(&addr,0,sizeof(addr));
   addr.sin_addr=*(struct in_addr *) host_entry->h_addr_list[0];
